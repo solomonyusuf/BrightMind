38b150a526a07b87e0a0adbcfe5f4475db85c0ba
Solomon yusuf <olafire03@gmail.com>
2025-07-01 12:17:50 +0100

    add commas

--- app/Livewire/Pages/Dashboard.php
+++ app/Livewire/Pages/Dashboard.php
@@ -82,74 +82,74 @@ public function streamAIResponse()
                 SEE COURSES:
                     100 Level Courses
 
-                        CSC 112: PRINCIPLES OF COMPUTER ORGANIZATION (2 UNITS) - C
-                        CSC 113: COMPUTER APPLICATION I FOR ARTS, SOCIAL SCIENCES & MANAGEMENT SCIENCES (2 UNITS)
-                        CSC 120: COMPUTER AS A PROBLEM SOLVING TOOL (3 UNITS) - C
-                        CSC 132: PRINCIPLES OF PROGRAMMING LANGUAGES I (2 UNITS) - C
+                        CSC 112: PRINCIPLES OF COMPUTER ORGANIZATION (2 UNITS) - C,
+                        CSC 113: COMPUTER APPLICATION I FOR ARTS SOCIAL SCIENCES & MANAGEMENT SCIENCES (2 UNITS),
+                        CSC 120: COMPUTER AS A PROBLEM SOLVING TOOL (3 UNITS) - C,
+                        CSC 132: PRINCIPLES OF PROGRAMMING LANGUAGES I (2 UNITS) - C,
 
                         200 Level Courses
 
-                        CSC 201: COMPUTER APPLICATION II FOR ARTS, SOCIAL SCIENCES & MANAGEMENT SCIENCES (2 UNITS)
-                        CSC 204: INTRODUCTION TO DISCRETE MATHEMATICS (2 UNITS) - C
-                        CSC 205: OPERATING SYSTEM I (3 UNITS) - C
-                        CSC 208: INTRODUCTION TO COMPUTER HARDWARE LABORATORY (2 UNITS) - E
-                        CSC 212: COMPUTER ARCHITECTURE (3 UNITS) - C
-                        CSC 213: ALGORITHM DEVELOPMENT AND APPLICATION (3 UNITS) – C
-                        CSC 214: DATABASE DESIGN AND MANAGEMENT I (3 UNITS) - C
-                        CSC 215: SOFTWARE PRACTICE (2 UNITS)
-                        CSC 217: FUNDAMENTAL OF DIGITAL ELECTRONICS (2 UNITS) - R
-                        CSC 218: FOUNDATION OF SEQUENTIAL PROGRAM (2 UNITS) - C
-                        CSC 219: DIGITAL LOGIC DESIGN (2 UNITS) - E
-                        CSC 221: FUNDAMENTALS OF DATA STRUCTURES (3 UNITS) - C
-                        CSC 222: ASSEMBLY LANGUAGE PROGRAMMING (2 UNITS) - C
-                        CSC 223: INTRODUCTION TO INFORMATION PROCESSING METHODS (2 UNITS) - C
-                        CSC 226: OBJECT-ORIENTED PROGRAMMING I, C++ (3 UNITS) - C
-                        CSC 228: SOFTWARE PRACTICE II (2 UNITS)
+                        CSC 201: COMPUTER APPLICATION II FOR ARTS SOCIAL SCIENCES & MANAGEMENT SCIENCES (2 UNITS),
+                        CSC 204: INTRODUCTION TO DISCRETE MATHEMATICS (2 UNITS) - C,
+                        CSC 205: OPERATING SYSTEM I (3 UNITS) - C,
+                        CSC 208: INTRODUCTION TO COMPUTER HARDWARE LABORATORY (2 UNITS) - E,
+                        CSC 212: COMPUTER ARCHITECTURE (3 UNITS) - C,
+                        CSC 213: ALGORITHM DEVELOPMENT AND APPLICATION (3 UNITS) – C,
+                        CSC 214: DATABASE DESIGN AND MANAGEMENT I (3 UNITS) - C,
+                        CSC 215: SOFTWARE PRACTICE (2 UNITS),
+                        CSC 217: FUNDAMENTAL OF DIGITAL ELECTRONICS (2 UNITS) - R,
+                        CSC 218: FOUNDATION OF SEQUENTIAL PROGRAM (2 UNITS) - C,
+                        CSC 219: DIGITAL LOGIC DESIGN (2 UNITS) - E,
+                        CSC 221: FUNDAMENTALS OF DATA STRUCTURES (3 UNITS) - C,
+                        CSC 222: ASSEMBLY LANGUAGE PROGRAMMING (2 UNITS) - C,
+                        CSC 223: INTRODUCTION TO INFORMATION PROCESSING METHODS (2 UNITS) - C,
+                        CSC 226: OBJECT-ORIENTED PROGRAMMING I C++ (3 UNITS) - C,
+                        CSC 228: SOFTWARE PRACTICE II (2 UNITS),
 
                         300 Level Courses
 
-                        CSC 301: COMPUTER APPLICATION III FOR ARTS, SOCIAL SCIENCES & MANAGEMENT SCIENCES (2 UNITS)
-                        CSC 303: OBJECT-ORIENTED PROGRAMMING II, Java (3 UNITS) - C
-                        CSC 319: COMPILER CONSTRUCTION (3 UNITS) - C
-                        CSC 323: EVOLUTIONARY COMPUTATION (2 UNITS) - E
-                        CSC 325: OPERATING SYSTEM II (3 UNITS) - C
-                        CSC 327: DATABASE MANAGEMENT SYSTEM II (3 UNITS) - C
-                        CSC 333: COMPUTER RESEARCH METHODOLOGY (1 Unit) - R
-                        CSC 335: INTRODUCTION TO FORMAL LANGUAGES AND AUTOMATA THEORY (2 UNITS) - E
-                        CSC 339: SYSTEMS ANALYSIS AND DESIGN (3 UNITS) - C
-                        CSC 371: FUNCTIONAL PROGRAMMING: (2 Units) - E
-                        CSC 392: PRACTICAL APPLICATION OF SOFTWARE DEVELOPMENT IN INDUSTRIES (4 UNITS) - R
-                        CSC 394: PRACTICAL APPLICATION OF DATABASE MANAGEMENT IN INDUSTRIES (4 UNITS) - R
-                        CSC 396: PRACTICAL APPLICATION OF DATA AND INFORMATION PRESENTATION SKILLS (4 UNITS) - R
-                        CSC 398: STUDENTS' INDUSTRIAL WORK EXPERIENCE SCHEME (SIWES) (6 UNITS) - C
+                        CSC 301: COMPUTER APPLICATION III FOR ARTS SOCIAL SCIENCES & MANAGEMENT SCIENCES (2 UNITS),
+                        CSC 303: OBJECT-ORIENTED PROGRAMMING II Java (3 UNITS) - C,
+                        CSC 319: COMPILER CONSTRUCTION (3 UNITS) - C,
+                        CSC 323: EVOLUTIONARY COMPUTATION (2 UNITS) - E,
+                        CSC 325: OPERATING SYSTEM II (3 UNITS) - C,
+                        CSC 327: DATABASE MANAGEMENT SYSTEM II (3 UNITS) - C,
+                        CSC 333: COMPUTER RESEARCH METHODOLOGY (1 Unit) - R,
+                        CSC 335: INTRODUCTION TO FORMAL LANGUAGES AND AUTOMATA THEORY (2 UNITS) - E,
+                        CSC 339: SYSTEMS ANALYSIS AND DESIGN (3 UNITS) - C,
+                        CSC 371: FUNCTIONAL PROGRAMMING: (2 Units) - E,
+                        CSC 392: PRACTICAL APPLICATION OF SOFTWARE DEVELOPMENT IN INDUSTRIES (4 UNITS) - R,
+                        CSC 394: PRACTICAL APPLICATION OF DATABASE MANAGEMENT IN INDUSTRIES (4 UNITS) - R,
+                        CSC 396: PRACTICAL APPLICATION OF DATA AND INFORMATION PRESENTATION SKILLS (4 UNITS) - R,
+                        CSC 398: STUDENTS' INDUSTRIAL WORK EXPERIENCE SCHEME (SIWES) (6 UNITS) - C,
 
                         400 Level Courses
 
-                        CSC 405: SOCIAL ISSUES IN INFORMATION TECHNOLOGY (2 UNITS) - E
-                        CSC 413: SOFTWARE ENGINEERING (4 UNITS) - C
-                        CSC 418: PATTERN RECOGNITION AND COMPUTER VISION (3 UNITS) - E
-                        CSC 419: STATISTICAL COMPUTING (3 UNITS) - E
-                        CSC 420: INTRODUCTION TO COMPUTER SECURITY (3 UNITS) - C
-                        CSC 421: EMBEDDED COMPUTER SYSTEMS (2 UNITS) - E
-                        CSC 424: EXPERT SYSTEMS AND KNOWLEDGE ENGINEERING (2 UNITS) - E
-                        CSC 426: FURTHER STATISTICAL PROCESSING (3 UNITS) - E
-                        CSC 427: COMPUTATIONAL SCIENCE AND NUMERICAL ANALYSIS (3 UNITS) - C
-                        CSC 428: COMPUTER GRAPHICS AND VISUAL COMPUTING (2 UNITS) - E
-                        CSC 429: MODELING AND SIMULATION (3 UNITS) - E
-                        CSC 431: ANALYSIS AND DESIGN OF DIGITAL SYSTEM (3 UNITS) - E
-                        CSC 432: PRINCIPLES OF PROGRAMMING LANGUAGES II (3 UNITS) - C
-                        CSC 433: OPTIMIZATION THEORY (3 UNITS) - E
-                        CSC 437: ARTIFICIAL INTELLIGENCE (3 UNITS) - C
-                        CSC 438: COMPUTER NETWORK AND DATA COMMUNICATION (3 UNITS) - E
-                        CSC 439: STATISTICAL PROCESSING SYSTEMS (3 UNITS) - E
-                        CSC 441: PROJECT MANAGEMENT (3 UNITS)
-                        CSC 442: INTRODUCTION TO PHP AND MYSQL (2 UNITS) - E
-                        CSC 444: MANAGEMENT INFORMATION SYSTEM
-                        CSC 451: HUMAN COMPUTER INTERFACE (HCI) (2 UNITS) - C
-                        CSC 452: ROBOTICS: (2 UNITS) - E
-                        CSC 454: DIGITAL IMAGE PROCESSING (2 UNITS) - E
-                        CSC 455: NET-CENTRIC COMPUTING (3 UNITS) - E
-                        CSC 497: SEMINAR ON SPECIAL TOPICS IN COMPUTER SCIENCE (2 UNITS) - C
+                        CSC 405: SOCIAL ISSUES IN INFORMATION TECHNOLOGY (2 UNITS) - E,
+                        CSC 413: SOFTWARE ENGINEERING (4 UNITS) - C,
+                        CSC 418: PATTERN RECOGNITION AND COMPUTER VISION (3 UNITS) - E,
+                        CSC 419: STATISTICAL COMPUTING (3 UNITS) - E,
+                        CSC 420: INTRODUCTION TO COMPUTER SECURITY (3 UNITS) - C,
+                        CSC 421: EMBEDDED COMPUTER SYSTEMS (2 UNITS) - E,
+                        CSC 424: EXPERT SYSTEMS AND KNOWLEDGE ENGINEERING (2 UNITS) - E,
+                        CSC 426: FURTHER STATISTICAL PROCESSING (3 UNITS) - E,
+                        CSC 427: COMPUTATIONAL SCIENCE AND NUMERICAL ANALYSIS (3 UNITS) - C,
+                        CSC 428: COMPUTER GRAPHICS AND VISUAL COMPUTING (2 UNITS) - E,
+                        CSC 429: MODELING AND SIMULATION (3 UNITS) - E,
+                        CSC 431: ANALYSIS AND DESIGN OF DIGITAL SYSTEM (3 UNITS) - E,
+                        CSC 432: PRINCIPLES OF PROGRAMMING LANGUAGES II (3 UNITS) - C,
+                        CSC 433: OPTIMIZATION THEORY (3 UNITS) - E,
+                        CSC 437: ARTIFICIAL INTELLIGENCE (3 UNITS) - C,
+                        CSC 438: COMPUTER NETWORK AND DATA COMMUNICATION (3 UNITS) - E,
+                        CSC 439: STATISTICAL PROCESSING SYSTEMS (3 UNITS) - E,
+                        CSC 441: PROJECT MANAGEMENT (3 UNITS),
+                        CSC 442: INTRODUCTION TO PHP AND MYSQL (2 UNITS) - E,
+                        CSC 444: MANAGEMENT INFORMATION SYSTEM,
+                        CSC 451: HUMAN COMPUTER INTERFACE (HCI) (2 UNITS) - C,
+                        CSC 452: ROBOTICS: (2 UNITS) - E,
+                        CSC 454: DIGITAL IMAGE PROCESSING (2 UNITS) - E,
+                        CSC 455: NET-CENTRIC COMPUTING (3 UNITS) - E,
+                        CSC 497: SEMINAR ON SPECIAL TOPICS IN COMPUTER SCIENCE (2 UNITS) - C,
 
                         Legend
 
@@ -165,7 +165,7 @@ public function streamAIResponse()
                 - Use line breaks for readability but avoid special formatting characters
                 - Present information in a natural, readable format suitable for direct display to user
                 - when writing the courses use Uppercase to write them
-                -Before responding, verify All course names are complete, Unit calculations are correct , Sentences are properly formed
+                - Before responding, verify All course names are complete, Unit calculations are correct , Sentences are properly formed
                 - I do not need to recorrect or reinstruct you to do the right thing
                 "
                         ]
